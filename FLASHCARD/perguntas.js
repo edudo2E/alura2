ee0cb858a48265d5ca75a52e52131fcaf7fe70b3
edudo2E <luisjesus@professor.educacao.sp.gov.br>
2024-10-22 07:39:35 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'QUAL É A COMBINAÇÃO DA TECNOLOGIA E DA MATEMÁTICA?',
+    'A TEC É A MAT SÃO ENSINADAS PELO MESMO PROF'
+)
+
+criaCartao(
+    'pessoal',
+    'POR QUE A MATERIA DE ARTES É ENSINADO ATE 0 1ª DO EM?',
+    'DEPOIS DO 1º, CRIASSE NOVAS MATÉRIAS'
+)
+
+criaCartao(
+    'recomendação',
+    'POR QUE OS ALUNOS DEVEM ESTUDAR?',
+    'PARA SE TORNAR ALGUEM NA VIDA'
+)
+
+criaCartao(
+    'recomendacão',
+    'O QUE FAZER QUANDO ESTÁ ESTRESSADO?',
+    'RESPIRA FUNDO E NÃO PERDER A PACIENCIA'
+)
